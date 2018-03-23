ebe412b257b620feb93436ed1241016f67e1b92d
Chris Smoak <csmoak@schoorunner.org>
2018-03-22 23:30:36 -0600

    Style fixes. Product creator.

---
+++ product-creator/index.js
@@ -0,0 +1,142 @@
+const _ = require('lodash');
+const combos = require('combos');
+const WooCommerceAPI = require('woocommerce-api');
+
+const rawProductsData = require('./products.json');
+const WooCommerce = new WooCommerceAPI({
+    url: 'http://localhost:9000/',
+    consumerKey: 'ck_eda621663c4c50b2bc0853c8b014a96278c99400',
+    consumerSecret: 'cs_f4212c333683e44ad553ec193a66f0f6a9d5c0cf',
+    wpAPI: true,
+    version: 'wc/v2'
+});
+
+init();
+
+async function init() {
+    console.log('Product Creator Result');
+    console.log('-------------------------------------------');
+    const products = await getAllProducts();
+    const productsKeyedByName = _.keyBy(products, 'sku');
+    const createdAndUpdatedProducts = await createAllProducts(rawProductsData, productsKeyedByName);
+
+    createdAndUpdatedProducts.map(product => {
+        const rawData = _.find(rawProductsData, { sku: product.sku });
+        const skuBase = product.sku;
+        const names = _.map(product.attributes, 'name');
+        const options = _.map(product.attributes, 'options');
+        const variations = combos(_.zipObject(names, options));
+
+        variations.map(variation => {
+            const attributes = _.map(variation, (option, name) => ({ name, option }));
+            const sku = skuBase + _.chain(variation)
+                .values()
+                .map(value => {
+                    const pieces = _.split(value, '=');
+                    return pieces[0];
+                })
+                .join('')
+                .value();
+
+            const definedVariation = _.find(rawData.variations, { sku });
+            if (definedVariation) {
+                const variationData = _.extend({}, definedVariation, { attributes });
+                createOrUpdateProductVariation(product.id, variationData);
+            } else {
+                // console.log(' - skipping variation: ' + sku);
+            }
+        });
+    });
+}
+
+function getAllProducts() {
+    return new Promise((resolve, reject) => {
+        WooCommerce.get('products?per_page=100', function(err, data, res) {
+            if (err) {
+                return reject(err);
+            }
+            resolve(JSON.parse(res));
+        });
+    });
+}
+
+function createAllProducts(rawProductsData, productsKeyedByName) {
+    const createOrUpdatePromises = rawProductsData.map(productData => {
+        if (_.has(productsKeyedByName, productData.sku)) {
+            const product = _.get(productsKeyedByName, productData.sku);
+            return updateProduct(product.id, productData);
+        }
+        return createProduct(productData);
+    });
+
+    return Promise.all(createOrUpdatePromises);
+}
+
+function createProduct(productData) {
+    return new Promise((resolve, reject) => {
+        WooCommerce.post('products', productData, function(err, data, res) {
+            if (err) {
+                return reject(err);
+            }
+            console.log('Created: ' + productData.name);
+            resolve(JSON.parse(res));
+        });
+    });
+}
+
+function updateProduct(productId, productData) {
+    return new Promise((resolve, reject) => {
+        WooCommerce.post('products/' + productId, productData, function(err, data, res) {
+            if (err) {
+                return reject(err);
+            }
+            console.log('Updated: ' + productData.name);
+            resolve(JSON.parse(res));
+        });
+    });
+}
+
+async function createOrUpdateProductVariation(productId, productVariationData) {
+    const sku = productVariationData.sku;
+    const existingVariation = await getProductVariation(productId, sku);
+    if (existingVariation.length) {
+        updateProductVariation(productId, existingVariation[0].id, productVariationData);
+    } else {
+        createProductVariation(productId, productVariationData);
+    }
+}
+
+function getProductVariation(productId, variationSku) {
+    return new Promise((resolve, reject) => {
+        WooCommerce.get('products/' + productId + '/variations?sku=' + variationSku, function(err, data, res) {
+            if (err) {
+                return reject(err);
+            }
+            resolve(JSON.parse(res));
+        });
+    });
+}
+
+function createProductVariation(productId, productVariationData) {
+    return new Promise((resolve, reject) => {
+        WooCommerce.post('products/' + productId + '/variations', productVariationData, function(err, data, res) {
+            if (err) {
+                return reject(err);
+            }
+            console.log(' - created variation: ' + productVariationData.sku);
+            resolve(JSON.parse(res));
+        });
+    });
+}
+
+function updateProductVariation(productId, variationId, productVariationData) {
+    return new Promise((resolve, reject) => {
+        WooCommerce.put('products/' + productId + '/variations/' + variationId, productVariationData, function(err, data, res) {
+            if (err) {
+                return reject(err);
+            }
+            console.log(' - updated variation: ' + productVariationData.sku);
+            resolve(JSON.parse(res));
+        });
+    });
+}
